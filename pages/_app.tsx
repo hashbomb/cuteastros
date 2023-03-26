0b92404a2bfea38b0e33b66ba2b392a81b2299f7
clubdrop <hash-bomb@outlook.com>
2023-03-26 15:29:16 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { PolygonZkevmTestnet } from "@thirdweb-dev/chains";
 import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
 
-const activeChainId = <ChainId className="polygon-zkevm-testnet"></ChainId>;
-
-function MyApp({ Component, pageProps }: AppProps) {
-    return (
-      <Layout>
-        <ThirdwebProvider desiredChainId={activeChainId}>
-          <Component {...pageProps} activeChainId={activeChainId} />
-        </ThirdwebProvider>
-      </Layout>
-    )
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider activeChain={PolygonZkevmTestnet}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
