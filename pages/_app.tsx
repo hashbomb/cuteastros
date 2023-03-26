ccbdce626117882928f3e45827d300083f2ab216
clubdrop <hash-bomb@outlook.com>
2023-03-26 15:32:35 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import type { AppProps } from 'next/app'
 import { PolygonZkevmTestnet } from "@thirdweb-dev/chains";
 import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThirdwebProvider activeChain={PolygonZkevmTestnet}>
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+const activeChainId = polygon-zkevm-testnet;
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+      <Layout>
+        <ThirdwebProvider activeChain={PolygonZkevmTestnet}>
+          <Component {...pageProps} />
+        </ThirdwebProvider>
+      </Layout>
+    )
 }
 
-export default MyApp;
+export default MyApp
