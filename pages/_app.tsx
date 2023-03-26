9ce2f37970b360024550fcb35083e0e08e583e74
clubdrop <hash-bomb@outlook.com>
2023-03-26 15:35:26 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
 
-const activeChainId = polygon-zkevm-testnet;
+const activeChainId = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
